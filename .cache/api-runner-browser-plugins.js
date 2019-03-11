051a1de340e5ba5fc173e8b80312a2b0887d381e
Mehvish Saleem <Mehvish@Mehvishs-MacBook-Pro.local>
2019-03-11 01:06:19 -0700

    updated content

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/Mehvish/Documents/SFU/Semester5/Repo/gatsby/MehvishSaleem.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/Mehvish/Documents/SFU/Semester5/Repo/gatsby/MehvishSaleem.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ src/components/HeaderGeneric.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HeaderGeneric = (props) => (
+    <header id="header">
+        <h1>My musings</h1>
+        <p>Random little things</p>
+    </header>
+)
+
+export default HeaderGeneric
