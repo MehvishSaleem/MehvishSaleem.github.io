733f3e9966a7f6daabdd9d3bafee89b551d7e985
Mehvish Saleem <Mehvish@Mehvishs-MacBook-Pro.local>
2019-03-11 01:10:21 -0700

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/Mehvish/Documents/SFU/Semester5/Repo/gatsby/MehvishSaleem.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/Mehvish/Documents/SFU/Semester5/Repo/gatsby/MehvishSaleem.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
